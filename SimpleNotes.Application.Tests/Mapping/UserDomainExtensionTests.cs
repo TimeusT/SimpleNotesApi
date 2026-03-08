b8c36b5a3f7ae8d0f4d4b7ff9b4b513376982dcd
TimeusT <littlemonkies29@gmail.com>
2026-03-08 17:47:39 +1000

    Feat: React TicTacToe
    SimpleNotes.UI React minigame implemented.
    ! Have not added time traveling

--- SimpleNotes.Application.Tests/Mapping/UserDomainExtensionTests.cs
+++ SimpleNotes.Application.Tests/Mapping/UserDomainExtensionTests.cs
@@ -36,16 +36,6 @@ public class NoteDomainExtensionsTests
     public void Given_ValidUserDomain_When_ToEntity_Then_ReturnUserItemEntity()
     {
         // Assign
-        var validNote = new NoteItemEntity
-        {
-            Id = 1,
-            Title = "Test Note",
-            Content = "This is a test note",
-            CreatedAt = DateTime.UtcNow,
-            LastUpdatedAt = DateTime.UtcNow,
-            UserId = 1
-        };
-
         var validDomain = new UserDomain
         (
             "John",
@@ -53,8 +43,7 @@ public class NoteDomainExtensionsTests
             30,
             DateTime.Now,
             1,
-            EmailText.Create("John@Doe.com"),
-            new List<NoteItemEntity> { validNote }
+            EmailText.Create("John@Doe.com")
         );
 
         // Act
