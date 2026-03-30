638e9e44dfdde4942ed176e876db4602170fe745
Tom Banh <tom.banh@gmail.com>
2026-03-30 17:00:40 +1000

    Feat: Updated notepad notes

--- SimpleNotes.UI/src/components/Layout.tsx
+++ SimpleNotes.UI/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 
 export default function Layout() {
   return (
