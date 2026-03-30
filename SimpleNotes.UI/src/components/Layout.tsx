7512abf90b89f970e1895d90640dab5997e9a596
Tom Banh <tom.banh@gmail.com>
2026-03-30 14:48:51 +1000

    Feat: fix form layout

--- SimpleNotes.UI/src/components/Layout.tsx
+++ SimpleNotes.UI/src/components/Layout.tsx
@@ -4,11 +4,19 @@ import { Box, Container } from "@mui/material";
 
 export default function Layout() {
   return (
-    <>
-    <Navbar/>
+    <Container
+      maxWidth="lg"
+      disableGutters // removes left/right padding
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Navbar />
       <main>
         <Outlet /> {/* This is where Home, CreateUser, etc., will render */}
       </main>
-    </> 
+    </Container>
   );
 }
