84ade8ccb31f8b4a7c80f2a5173ffe56b6f280f4
TimeusT <littlemonkies29@gmail.com>
2026-07-04 12:32:35 +1000

    Feat: Improved CreateAsync test for note controller

--- SimpleNotes.Api.Tests/NoteControllerTests.cs
+++ SimpleNotes.Api.Tests/NoteControllerTests.cs
@@ -9,24 +9,66 @@ namespace SimpleNotes.Api.Tests;
 
 public class NoteControllerTests
 {
+    //[Fact]
+    //public void Given_ValidCreateNoteRequest_When_CreateNote_Then_ReturnCreatedAtAction()
+    //{
+    //    // Assign or Arrange
+    //    // Mock the Service Interface
+    //    var mockNoteService = new Mock<INoteService>();
+    //    // Mock the create method
+    //    var mockCreateNote = new NoteDomain(
+    //        AlphaText.Create("Test"),
+    //        1,
+    //        AlphaText.Create("This is a test note"),
+    //        1
+    //        );
+    //    // It.IsAny is a Moq feature that matches any 'NoteDomain' object passed to the 'Create' method
+    //    mockNoteService.Setup(service => service.Create(It.IsAny<NoteDomain>())).Returns(mockCreateNote);
+
+    //    // Create controller with the mocked service
+    //    var controller = new NoteController(mockNoteService.Object);
+
+    //    var validNoteRequest = new CreateNoteRequest
+    //    {
+    //        Title = "Test",
+    //        Content = "Testing",
+    //        UserId = 1
+    //    };
+
+    //    // Act
+    //    var result = controller.Create(validNoteRequest);
+
+    //    // Assert
+    //    // Check for 201 response
+    //    var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+    //    // Check that the correct action was called
+    //    Assert.Equal("GetById", createdAtActionResult.ActionName);
+    //    // Ensure correct ID is passed
+    //    Assert.Equal(1, createdAtActionResult.RouteValues["id"]);
+    //}
+
     [Fact]
-    public void Given_ValidCreateNoteRequest_When_CreateNote_Then_ReturnCreatedAtAction()
+    public async Task Given_ValidCreateNoteRequest_When_CreateNote_Then_ReturnCreatedAtAction()
     {
-        // Assign or Arrange
-        // Mock the Service Interface
-        var mockNoteService = new Mock<INoteService>();
-        // Mock the create method
-        var mockCreateNote = new NoteDomain(
+        // Arrange
+        var validNote = new NoteDomain(
             AlphaText.Create("Test"),
             1,
-            AlphaText.Create("This is a test note"),
+            AlphaText.Create("Testing"),
             1
-            );
-        // It.IsAny is a Moq feature that matches any 'NoteDomain' object passed to the 'Create' method
-        mockNoteService.Setup(service => service.Create(It.IsAny<NoteDomain>())).Returns(mockCreateNote);
+        );
 
-        // Create controller with the mocked service
-        var controller = new NoteController(mockNoteService.Object);
+        var token = new CancellationToken();
+
+        var service = new Mock<INoteService>();
+
+        service.Setup(x => x.CreateAsync(
+            It.Is<NoteDomain>(n =>
+                n.Title.Value == "Test" &&
+                n.Content!.Value == "Testing" &&
+                n.UserId == 1),
+            token))
+            .ReturnsAsync(validNote);
 
         var validNoteRequest = new CreateNoteRequest
         {
@@ -35,15 +77,18 @@ public class NoteControllerTests
             UserId = 1
         };
 
+        var controller = new NoteController(service.Object);
+
         // Act
-        var result = controller.Create(validNoteRequest);
+        var result = await controller.CreateAsync(validNoteRequest, token);
 
         // Assert
-        // Check for 201 response
-        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
-        // Check that the correct action was called
-        Assert.Equal("GetById", createdAtActionResult.ActionName);
-        // Ensure correct ID is passed
-        Assert.Equal(1, createdAtActionResult.RouteValues["id"]);
+        var okResult = Assert.IsType<CreatedAtActionResult>(result);
+
+        var okValue = (NoteResponse)okResult.Value!;
+
+        Assert.Equal(validNote.Title, okValue.Title);
+        Assert.Equal(validNote.Content!, okValue.Content);
+        Assert.Equal(validNote.UserId, okValue.UserId);
     }
 }
